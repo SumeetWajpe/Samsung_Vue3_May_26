9811a3db42eb9fe631c7b6f5009273983499f84c
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2026-05-19 14:35:53 +0530

    ✨ onlineshoppingapp - Options API (Product component)

---
+++ onlineshoppingapp_optionsapi/src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import Product from './components/Product.vue';
+
+export default {
+  name: "App",
+  components: {
+    Product
+  }
+}
+
+</script>
+
+<template>
+  <Product name="Mobile" />
+  <Product name="Camera" />
+
+</template>
---
+++ onlineshoppingapp_optionsapi/src/components/Product.vue
@@ -0,0 +1,13 @@
+<template>
+    <h2>
+        {{ name }}
+    </h2>
+</template>
+<script>
+export default {
+    name: 'Product',
+    props: {
+        name: String,
+    }
+}
+</script>
