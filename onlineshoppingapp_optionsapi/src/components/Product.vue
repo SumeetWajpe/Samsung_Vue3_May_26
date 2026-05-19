e4658a33033ba9e9709c06cfb4e580308009ee02
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2026-05-19 17:40:04 +0530

    📝 class / style binding and disable button on quantity zero

--- onlineshoppingapp_optionsapi/src/components/Product.vue
+++ onlineshoppingapp_optionsapi/src/components/Product.vue
@@ -1,12 +1,21 @@
 <template>
-    <img :src="productdetails.imageUrl" v-bind:alt="productdetails.title" width="200">
-    <h2>
-        {{ productdetails.title }}
-    </h2>
-    <p>{{ productdetails.price }}</p>
-    <p>{{ productdetails.rating }}</p>
-    <button @click="IncrementLikes" class="btn btn-primary">{{ productdetails.likes }}</button>
-    <!-- <button v-on:click="IncrementCount">{{ count }}</button> -->
+    <!-- <div class="productItem" :class="['outofstock',]"> -->
+    <div class="productItem">
+
+        <img :src="productdetails.imageUrl" v-bind:alt="productdetails.title" width="200" height="100"
+            :style="productdetails.quantity ? '' : { opacity: 0.4 }">
+        <h2>
+            {{ productdetails.title }}
+        </h2>
+        <p>Rs. {{ productdetails.price }}</p>
+        <p>{{ productdetails.quantity }}</p>
+
+        <p>{{ productdetails.rating }}</p>
+        <button @click="IncrementLikes" class="btn btn-primary">{{ productdetails.likes }}</button>
+        <button class="btn btn-warning mx-1" :disabled="!productdetails.quantity">Add to cart</button>
+        <!-- <button v-on:click="IncrementCount">{{ count }}</button> -->
+
+    </div>
 </template>
 <script>
 export default {
@@ -32,3 +41,16 @@ export default {
     }
 }
 </script>
+<style>
+.productItem {
+    border: 1px solid grey;
+    border-radius: 5px;
+    box-shadow: 5px 5px 5px darkgray;
+    padding: 10px;
+    margin: 5px;
+}
+
+.outofstock {
+    opacity: 0.4;
+}
+</style>
