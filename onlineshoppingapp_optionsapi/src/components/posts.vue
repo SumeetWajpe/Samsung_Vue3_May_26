e7ad5383a00b92669ddf3904628c4833779609ad
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2026-05-20 17:43:26 +0530

    ✨ Displaying posts in UI (AJAX)

--- onlineshoppingapp_optionsapi/src/components/posts.vue
+++ onlineshoppingapp_optionsapi/src/components/posts.vue
@@ -1,16 +1,33 @@
 <template>
     <div>
         <h3>All Posts</h3>
+
+        <div v-if="loading">
+            Loading....
+        </div>
+        <div v-else>
+            <ul class="list-group">
+                <li class="list-group-item" v-for="post in postList">{{ post.title }}</li>
+            </ul>
+        </div>
+
     </div>
 </template>
 
 <script>
 export default {
     name: "Posts",
+    data() {
+        return {
+            postList: [],
+            loading: false
+        }
+    },
     methods: {
         fetchPosts() {
+            this.loading = true;
             fetch("https://jsonplaceholder.typicode.com/posts").then(response => response.json())
-            .then(posts => console.log(posts))
+                .then(posts => this.postList = posts).catch(err => console.log(err)).finally(() => this.loading = false);
         },
     },
 
