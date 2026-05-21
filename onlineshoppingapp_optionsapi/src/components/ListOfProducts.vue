92596c321ed4209283984abd483030864274cd51
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2026-05-21 15:23:05 +0530

    ✨ Getting products using Product Service

--- onlineshoppingapp_optionsapi/src/components/ListOfProducts.vue
+++ onlineshoppingapp_optionsapi/src/components/ListOfProducts.vue
@@ -14,11 +14,12 @@
 
 
 
-    </div>o
+    </div>
 
 </template>
 
 <script>
+import ProductService from '../services/product.service';
 import Product from './Product.vue';
 
 export default {
@@ -28,48 +29,7 @@ export default {
     },
     data() {
         return {
-            message: "Hello",
-            products: [{
-                id: 1,
-                title: "Laptop",
-                price: 50000,
-                quantity: 100,
-                rating: 4,
-                imageUrl: "https://techterms.com/img/xl/laptop_586.png",
-                likes: 100
-            }, {
-                id: 2,
-                title: "OLED TV",
-                price: 25000,
-                quantity: 0,
-                rating: 3.5,
-                imageUrl: "https://www.pngkey.com/png/full/280-2808973_shop-our-experts-love-range-of-oled-tvs.png",
-                likes: 500
-            }, {
-                id: 3,
-                title: "Desktop",
-                price: 10000,
-                quantity: 200,
-                rating: 3,
-                imageUrl: "https://www.pngmart.com/files/7/Desktop-Computer-Transparent-Background.png",
-                likes: 200
-            }, {
-                id: 4,
-                title: "Mobile",
-                price: 20000,
-                quantity: 1000,
-                rating: 5,
-                imageUrl: "https://static.toiimg.com/photo/73078527.cms",
-                likes: 400
-            }, {
-                id: 5,
-                title: "Camera",
-                price: 90000,
-                quantity: 0,
-                rating: 4,
-                imageUrl: "https://images-na.ssl-images-amazon.com/images/I/813CAX2%2BtQL._SL1500_.jpg",
-                likes: 100
-            }]
+            products: []
         }
     },
     methods: {
@@ -83,7 +43,18 @@ export default {
             // console.log("Deleting the product.."+theId)
             let index = this.products.findIndex(p => p.id == theId);
             this.products.splice(index, 1);
+        },
+        fetchProducts() {
+            console.log("fetchProducts")
+            let thePromise = ProductService.getProducts();
+            thePromise.then(products => {
+                this.products = products;
+                console.log(products)
+            }).catch(err => console.log(err));
         }
+    },
+    mounted() {
+        this.fetchProducts();
     }
 }
 </script>
