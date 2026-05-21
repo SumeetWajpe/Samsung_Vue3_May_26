725bd28c1c492366de0e59dc4eb4c81b60f69dcb
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2026-05-22 05:12:36 +0530

    ✨ Onlineshopping app with composition API

--- onlineshoppingapp_compositionapi/src/App.vue
+++ onlineshoppingapp_compositionapi/src/App.vue
@@ -2,11 +2,12 @@
 // import Counter from './components/Counter.vue'
 // import ComputedInComp from './components/ComputedProp.vue'
 // import Watchers from './components/Watchers.vue'
-import Reactive from "./components/Reactive.vue"
+// import Reactive from "./components/Basics/Reactive.vue"
+import ListOfProducts from "./components/ListOfProducts.vue"
 
 
 </script>
 
 <template>
-  <Reactive />
+  <ListOfProducts />
 </template>
---
+++ onlineshoppingapp_compositionapi/src/components/Product.vue
@@ -0,0 +1,64 @@
+<template>
+    <!-- <div class="productItem" :class="['outofstock',]"> -->
+    <!-- <div class="productItem" :class="isSelected ? 'selectedItem' : ''"> -->
+    <!-- <div class="productItem" :class="classesToBeApplied"> -->
+    <div class="productItem" :class="{ outofstock: !productdetails.quantity, selectedItem: isSelected }">
+
+
+
+        <img :src="productdetails.imageUrl" v-bind:alt="productdetails.title" width="200" height="100"
+            :style="productdetails.quantity ? '' : { opacity: 0.4 }">
+        <h2>
+            {{ productdetails.title }}
+        </h2>
+        <p>Rs. {{ productdetails.price }}</p>
+        <p :class="productdetails.quantity ? 'text-success' : 'text-danger fw-bold'">{{
+            outofstock(productdetails.quantity, 'nos') }}</p>
+
+        <p>{{ productdetails.rating }}</p>
+        <p>
+            <input type="checkbox" :id="'chkCart-' + productdetails.id" v-model="isSelected"> <label
+                :for="'chkCart-' + productdetails.id">Add To
+                Wishlist</label>
+        </p>
+        <button @click="IncrementLikes" class="btn btn-primary">{{ productdetails.likes }}</button>
+        <button class="btn btn-warning mx-1" :disabled="!productdetails.quantity">Add to cart</button>
+        <button class="btn btn-outline-danger">Delete</button>
+
+    </div>
+</template>
+<script setup>
+import { defineProps, ref } from 'vue';
+const props = defineProps({
+    productdetails: {
+        type: Object,
+        required: true
+    }
+});
+
+var isSelected = ref(false);
+
+function IncrementLikes() {
+    props.productdetails.likes += 1;
+};
+function outofstock(value, text) {
+    return value > 0 ? `${value} ${text}` : `Out Of Stock`;
+};
+</script>
+<style>
+.productItem {
+    border: 1px solid grey;
+    border-radius: 10px;
+    box-shadow: 5px 5px 5px darkgray;
+    padding: 10px;
+    margin: 5px;
+}
+
+.selectedItem {
+    background-color: lightblue;
+}
+
+.outofstock {
+    opacity: 0.4;
+}
+</style>
