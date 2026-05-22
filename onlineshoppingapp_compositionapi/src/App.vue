f8dae4ee33213007ea2788bad573ec018e59048f
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2026-05-22 17:08:43 +0530

    ✨ Adding Router Links

--- onlineshoppingapp_compositionapi/src/App.vue
+++ onlineshoppingapp_compositionapi/src/App.vue
@@ -5,5 +5,15 @@
 </script>
 
 <template>
+  <!-- Page refreshes -->
+
+  <!-- <a href="/">Products</a> |
+  <a href="/posts">Posts</a> -->
+
+  <nav>
+    <RouterLink to="/">Products</RouterLink> | 
+    <RouterLink to="/posts">Posts</RouterLink>
+  </nav>
+
   <RouterView />
 </template>
